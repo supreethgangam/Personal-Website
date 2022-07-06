17d24c42b82c95d3ee0f9b26e17f92c6566277e2
supreethgangam <supreeth.gangam@gmail.com>
2022-07-06 20:18:30 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import pic from '../assets/png/profpic.jpg'
 export const headerData = {
     name: 'Supreeth Reddy Gangam',
     title: "Student",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:"When life gives you lemonade, make lemons. Life will be all like \"What?!\"",
     image: pic,
     resumePdf: resume
 }
