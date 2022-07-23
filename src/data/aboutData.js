c3f773b79dcc9b9ab6e57146b0043339041b3f88
supreethgangam <supreeth.gangam@gmail.com>
2022-07-23 17:14:24 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "About me",
     description1: "My name's Supreeth. I'm an undergraduate student pursuing Computer Science and Engineering at International Institute of Information Technology, Hyderabad.",
-    description2: "I'm currently looking for summer internships with a duration of 2-3 months. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description2: "I have a good knowledge and experience in web development. I have a keen interest in competitive programming. I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
     image: 2
 }
